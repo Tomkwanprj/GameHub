44cd5b905a8c50af264b9daf2927108c72cc8eda
Tom Kwan <tomkwanyingkin@example.com>
2023-11-21 22:42:26 +0800

    feat: add genre icons to the genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
+import { HStack, Icon, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading, setData, setErrors, setIsLoading } =
     useGenres();
 
   return (
-    <ul>
-      {data.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+    <List>
+      {data.map(({ id, name, image_background }) => (
+        <ListItem paddingY={"5px"} key={id}>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text fontSize={"lg"}>{name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
