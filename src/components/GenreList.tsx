3a92c9148813dce1e16bcac3ee8fd2cc89f69b9b
Tom Kwan <tomkwanyingkin@example.com>
2023-11-25 16:25:53 +0800

    -feat: highlighting the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -13,9 +13,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (name: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, error, isLoading, setData, setErrors, setIsLoading } =
     useGenres();
 
@@ -34,6 +35,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 borderRadius={8}
               />
               <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant={"link"}
                 fontSize={"lg"}
